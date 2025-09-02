153eaa79919d78f0157f0f5475c094c28c69afd7
eccentricpark <pen40@naver.com>
2025-09-03 00:22:03 +0900

    파일 오타 수정

--- src/features/resizing-conversion/resizing-conversion.controller.ts
+++ src/features/resizing-conversion/resizing-conversion.controller.ts
@@ -3,7 +3,7 @@ import { JsonController, Post, UploadedFiles, Body } from 'routing-controllers';
 import { Express } from 'express';
 import { Service } from 'typedi';
 import { memoryOption } from '../../shared/multer';
-import { ResizingConversionService } from './resizing-conversion.service.';
+import { ResizingConversionService } from './resizing-conversion.service';
 import { ResizeOptions } from '../../shared/sharpUtils';
 
 @Service()
@@ -15,6 +15,21 @@ export class ResizingConversionController {
 
   @Post('/multiple')
   async resizeToConvertFiles(
+    @UploadedFiles('images', memoryOption) files: Express.Multer.File[],
+    @Body() options: ResizeOptions){
+    return await this.service.resizeToConvertFiles(files, options);
+  }
+
+  /**
+   * 파일 다운로드 기능을 추가한다.
+   * 기존의 앤드포인트들은 postman에서 테스트했음.
+   * 
+   * 이 앤드포인트는 웹페이지에서 이미지 파일 업로드 시,
+   * width, height에 맞춰 조절 및 jpg 변환 후, zip파일로 압축하여
+   * 다운로드 기능을 지원하도록 구현해야 한다.
+   */
+  @Post('/multiple-download')
+  async downloadFiles(
     @UploadedFiles('images', memoryOption) files: Express.Multer.File[],
     @Body() options: ResizeOptions){
     return await this.service.resizeToConvertFiles(files, options);
